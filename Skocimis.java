578d694fa325fbf14425a41b5cef0ae355f80f8c
IT5706021630071 <5706021630071@fitm.kmutnb.ac.th>
2016-02-15 22:15:17 +0700

    Update Skocimis.java

--- Skocimis.java
+++ Skocimis.java
@@ -1,28 +1,33 @@
-import java.util.Scanner;
-
+import javax.swing.JOptionPane;
 public class Skocimis {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		int n1, n2, n3;
-		Scanner scan = new Scanner(System.in);
+		String s1,s2,s3,s4,s5,s6;
 		do {
-			System.out.print("Enter 3 integer (0 < Number1 < Number2 < Number3 < 100):");
-			n1 = scan.nextInt();
-			n2 = scan.nextInt();
-			n3 = scan.nextInt();
+			s1=JOptionPane.showInputDialog("Enter 3 integer :");
+			n1 = Integer.parseInt(s1);
+			n2 = Integer.parseInt(s1);
+			n3 = Integer.parseInt(s1);
 			if (n1 <= 0 || n1 >= 100) {
-				System.out.println("Invalid Input. Please enter again!!!");
+				s2=JOptionPane.showInputDialog("Invalid Input. Please enter again!!!");
 			} else if (n2 <= 0 || n2 >= 100) {
-				System.out.println("Invalid Input. Please enter again!!!");
+				s3=JOptionPane.showInputDialog("Invalid Input. Please enter again!!!");
 			} else if (n3 <= 0 || n3 >= 100) {
-				System.out.println("Invalid Input. Please enter again!!!");
+				s4=JOptionPane.showInputDialog("Invalid Input. Please enter again!!!");
 			} else if (n2 <= n1) {
-				System.out.println("Number2 must more than Number1. Please enter again!!!");
+				s5=JOptionPane.showInputDialog("Number2 must more than Number1. Please enter again!!!");
 			} else if (n3 <= n2) {
-				System.out.println("Number3 must more than Number2. Please enter again!!!");
+				s6=JOptionPane.showInputDialog("Number3 must more than Number2. Please enter again!!!");
 			}
 		} while (n1 <= 0 || n1 >= 100 || n2 <= 0 || n2 >= 100 || n3 <= 0 || n3 >= 100 || n3 < n1 || n3 < n2 || n2 < n1
 				|| n1 > n2 || n1 > n3);
-		System.out.println("------- Output -------- ");
-		System.out.println(" Most played : " + ((n3 - n2) - 1) + " time.");
+		JOptionPane.showMessageDialog(null," Most played : " + total(n1,n2,n3) + " time.",null, JOptionPane.INFORMATION_MESSAGE);
+	
 	}
-}
+		public static int total(int n1,int n2,int n3)
+		{
+		return(((n3 - n2) - 1));
+		}
+	}
+
