03f5d4281b3be6a4d9863f3fa10d8412ff9ac9e9
IT5706021630071 <5706021630071@fitm.kmutnb.ac.th>
2016-02-16 23:44:27 +0700

    Update Skocimis.java

--- Skocimis.java
+++ Skocimis.java
@@ -3,22 +3,24 @@ public class Skocimis {
 	public static void main(String[] args) 
 	{
 		int n1, n2, n3;
-		String s1,s2,s3,s4,s5,s6;
+		String s1,s2,s3;
 		do {
-			s1=JOptionPane.showInputDialog("Enter 3 integer :");
+			s1=JOptionPane.showInputDialog("Enter integer :");
 			n1 = Integer.parseInt(s1);
-			n2 = Integer.parseInt(s1);
-			n3 = Integer.parseInt(s1);
+			s2=JOptionPane.showInputDialog("Enter integer :");
+			n2 = Integer.parseInt(s2);
+			s3=JOptionPane.showInputDialog("Enter integer :");
+			n3 = Integer.parseInt(s3);
 			if (n1 <= 0 || n1 >= 100) {
-				s2=JOptionPane.showInputDialog("Invalid Input. Please enter again!!!");
+				JOptionPane.showMessageDialog(null,"Invalid Input. Please enter again!!!",null, JOptionPane.INFORMATION_MESSAGE);
 			} else if (n2 <= 0 || n2 >= 100) {
-				s3=JOptionPane.showInputDialog("Invalid Input. Please enter again!!!");
+				JOptionPane.showMessageDialog(null,"Invalid Input. Please enter again!!!",null, JOptionPane.INFORMATION_MESSAGE);
 			} else if (n3 <= 0 || n3 >= 100) {
-				s4=JOptionPane.showInputDialog("Invalid Input. Please enter again!!!");
+				JOptionPane.showMessageDialog(null,"Invalid Input. Please enter again!!!",null, JOptionPane.INFORMATION_MESSAGE);
 			} else if (n2 <= n1) {
-				s5=JOptionPane.showInputDialog("Number2 must more than Number1. Please enter again!!!");
+				JOptionPane.showMessageDialog(null,"Number2 must more than Number1. Please enter again!!!",null, JOptionPane.INFORMATION_MESSAGE);
 			} else if (n3 <= n2) {
-				s6=JOptionPane.showInputDialog("Number3 must more than Number2. Please enter again!!!");
+				JOptionPane.showMessageDialog(null,"Number3 must more than Number2. Please enter again!!!",null, JOptionPane.INFORMATION_MESSAGE);
 			}
 		} while (n1 <= 0 || n1 >= 100 || n2 <= 0 || n2 >= 100 || n3 <= 0 || n3 >= 100 || n3 < n1 || n3 < n2 || n2 < n1
 				|| n1 > n2 || n1 > n3);
